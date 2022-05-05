e80ecf9d3324d4b53ce08c94174d3beca8d03bb8
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2022-05-05 00:46:24 -0300

    Add basic sound effects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,63 @@
+
+/*
+	Game core variables
+ */
+
 let player, winner = null;
 const playerSelected = document.getElementById('player-selected');
 const winnerSelected = document.getElementById('winner-selected');
 const squares = document.getElementsByClassName('square');
+
+/*
+	Utility variables
+ */
+
+	let audioParty, audioHeartbeat, audioLaugh, audioSiren;
+
+/*
+	Utility functions
+ */
+
+const loadSoundArquives = () => {
+
+  audioParty = new Audio(
+	"./media/mixkit-birthday-crowd-party-cheer-531.wav"
+  );
+
+  audioHeartbeat = new Audio(
+	// mixkit-cinematic-horror-heartbeat-transition-489.wav
+	"./media/mixkit-cinematic-horror-heartbeat-transition-489.wav"
+  );
+	
+//   audioLaugh = new Audio(
+// 	// mixkit-troll-warrior-laugh-409.wav
+// 	"./media/mixkit-troll-warrior-laugh-409.wav"
+//   );
+
+//   audioSiren = new Audio(
+// 	// mixkit-police-siren-us-1643.wav
+// 	"./media/mixkit-police-siren-us-1643.wav"
+//   );
+};
+
+
+const audioPlay = (a, loop, volume = 1) => {
+	a.play();
+	a.autoplay
+	if (loop) a.loop = true;
+	a.volume = volume;
+};
+  
+const audioStop = (a) => {
+	// https://codetogo.io/how-to-stop-audio-in-javascript/
+	a.pause();
+	a.currentTime = 0;
+};
+
+
+/*
+	Game core functions
+ */
 
 const chooseSquare = (id) => {
 	if (winner !== null) {
@@ -34,6 +90,8 @@ const changePlayer = (value) => {
 const changeWinner = (square) => {
 	winner = square.innerHTML;
 	winnerSelected.innerHTML = winner;
+	audioStop(audioHeartbeat);
+	audioPlay(audioParty, false, 0.5);
 }
 
 const changeSquareColor = (square1, square2, square3) => {
@@ -113,6 +171,10 @@ const checkSequence = (square1, square2, square3) => {
 }
 
 const restart = () => {
+	audioStop(audioHeartbeat);
+	audioPlay(audioHeartbeat, true, 0.2);
+	audioStop(audioParty);
+
 	winner = null;
 	winnerSelected.innerHTML = '';
 
@@ -126,4 +188,34 @@ const restart = () => {
 	changePlayer('X');
 }
 
-changePlayer('X');
+const init = () => {
+	loadSoundArquives();
+	changePlayer('X');
+
+	// Workaround for "Autoplay is only allowed when approved by the user, the site is activated by the user, or media is muted." issue.
+	// Check the (Todo) "sound effects 🎶🎸"" section in README.md
+	
+	// Get the modal
+	const modal = document.getElementById("myModal");
+
+	// Get the <span> element that closes the modal
+	const span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	modal.onclick = function() {
+		modal.style.display = "none";
+		audioPlay(audioHeartbeat, true, 0.2);
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target === modal) {
+			modal.style.display = "none";
+			audioPlay(audioHeartbeat, true, 0.2);
+		}
+	} 
+		
+}
+
+init();
+
